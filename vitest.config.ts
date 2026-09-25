0022af4f548494d18f43191a1971105fe3d87a59
Osmar Petry <osmarpetry@gmail.com>
2026-09-25 02:06:47 +0200

    chore: gh copolot sugestion

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./src/test/setup.ts'],
+    coverage: {
+      provider: 'v8'
+    }
+  }
+})
