fc4f787d1862baf893cf48189559a539bd2b0001
Osmar Petry <osmarpetry@gmail.com>
2026-09-25 02:12:11 +0200

    chore: ci updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 // vitest.config.ts
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'happy-dom',
